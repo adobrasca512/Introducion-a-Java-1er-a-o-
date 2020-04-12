ca8cde9a34f3530454ed97d1fdcc68e621d21f6a
Adilem Dobras <59998402+adobrasca512@users.noreply.github.com>
2020-04-12 22:51:13 +0200

    Hola Mundo- Introduccion a Java

--- Ejercicio_1.java
+++ Ejercicio_1.java
@@ -1,19 +1,11 @@
-package T3_Variables_Constantes;
+package T1_Introduccion_a_Java;
 
 public class Ejercicio_1 {
 
 	public static void main(String[] args) {
-	int valor1 = 8;
-	int valor2 = 3;
-	// Dadas dos variables enteras (inicializadas), calcular e imprimir por consola
-    int suma=valor1+valor2;
-    int resta=valor1-valor2;
-    int producto=valor1*valor2;
-    int media=valor1/valor2;
-    System.out.println("El resulado de la suma es: " +suma);
-    System.out.println("El resulado de la resta es: " +resta);
-    System.out.println("El resulado del producto es: " +producto);
-    System.out.println("El resulado de la media es: " +media);
+		// Hola mundo
+		System.out.println("Hola Mundo");
+
 	}
 
 }
